70575761694309dfaf7e59b757d7ecb701060914
YouKownWho <1106093641@qq.com>
2020-06-12 09:54:02 +0800

    iOS:  iPhone +1

---
+++ YKW/YKW/iOS/iPhoneManager.swift
@@ -0,0 +1,13 @@
+//
+//  iPhoneManager.swift
+//  YKW
+//
+//  Created by Light on 2020/6/12.
+//  Copyright © 2020 YouKnowWho. All rights reserved.
+//
+
+import Foundation
+
+class iPhoneManager {
+    // iPhone +1
+}
