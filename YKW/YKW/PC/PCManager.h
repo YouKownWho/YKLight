4d7e1d423991346f96e0536625b7bfe8bdcdc737
YouKownWho <1106093641@qq.com>
2020-06-12 10:08:10 +0800

    PC: +1

---
+++ YKW/YKW/PC/PCManager.h
@@ -0,0 +1,17 @@
+//
+//  PCManager.h
+//  YKW
+//
+//  Created by Light on 2020/6/12.
+//  Copyright © 2020 YouKnowWho. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PCManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YKW/YKW/PC/PCManager.m
@@ -0,0 +1,15 @@
+//
+//  PCManager.m
+//  YKW
+//
+//  Created by Light on 2020/6/12.
+//  Copyright © 2020 YouKnowWho. All rights reserved.
+//
+
+#import "PCManager.h"
+
+@implementation PCManager
+
+// PC: +1
+
+@end
